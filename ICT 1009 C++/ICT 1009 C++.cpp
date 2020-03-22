1571ebbaba2d85d52408c90a3708aea79ee3b6d4
puglord96 <55310319+puglord96@users.noreply.github.com>
2020-03-23 00:49:28 +0800

    did reddit crawling

--- ICT 1009 C++/ICT 1009 C++.cpp
+++ ICT 1009 C++/ICT 1009 C++.cpp
@@ -1,20 +1,57 @@
-// ICT 1009 C++.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <curl/curl.h>
+#include <nlohmann/json.hpp>
 
-int main()
+// for convenience
+using json = nlohmann::json;
+
+
+using namespace std;
+
+
+static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp)
 {
-    std::cout << "Hello World!\n";
+    ((string*)userp)->append((char*)contents, size * nmemb);
+    return size * nmemb;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int main(void)
+{
+    CURL* curl;
+    CURLcode res;
+    string readBuffer;
+    ofstream myfile;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    curl = curl_easy_init();
+    if (curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, "https://api.pushshift.io/reddit/search/comment/?q=kobe");
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+        res = curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
+
+
+       // cout << readBuffer << endl;
+        ofstream file("data.json");
+        file << readBuffer;
+        file.close();
+
+    }
+
+    std::ifstream jsonFile("data.json");
+    nlohmann::json commands;
+    jsonFile >> commands;
+ 
+    nlohmann::json data = commands["data"];
+    std::cout << "Number of items in Data: " << data.size() << std::endl;
+
+    json test = data[1];
+
+    for (auto it = test.begin(); it != test.end(); ++it)
+    {
+        std::cout << it.key() << ": " << it.value() << std::endl;
+    }
+    return 0;
+}
